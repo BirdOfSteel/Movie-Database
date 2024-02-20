80cb7dd2ca8118c9a306b5d24cb73f03f6d751e0
Furat <60301925+furappuccinos@users.noreply.github.com>
2024-02-19 19:47:37 -0500

    Updated path in index.css. Added rules to Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import {defineConfig} from 'vite'
-
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: []	
+	plugins: [],
+	build: {
+		assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg', '**/*.gif', '**/*.webp', '**/*.css']
+	}
 })
